fa3045e22eb45287be4fc4e28400e0f80db4510d
Israel Sales <israelsales043@gmail.com>
2023-09-15 14:49:17 -0300

    Correction bg.v4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { DefaultInput } from '../components/input';
 
 export default function Home() {
   return (
-    <body className="w-auto h-auto bg-[#09090B]">
+    <body className="w-auto h-auto bg-[#09090b]">
       <div className="flex justify-center items-center h-screen">
         <div className="flex flex-col bg-white rounded-3xl max-sm:w-72 max-lg:w-80 w-96">
           <h1 className="max-sm:text-xl max-lg:text-2xl text-3xl font-bold mt-8 mb-8 text-center">
