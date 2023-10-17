3325bc6c8c73e8a36053b07f6b8e44530eeff5e6
Israel Sales <israelsales043@gmail.com>
2023-10-17 17:13:31 -0300

    Teste Modal 1.2

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -39,42 +39,45 @@ export default function home() {
   return (
     <main className="min-h-screen w-scren bg-[#121214]">
       <DefaultHeader>
-        <div className=" flex flex-row-reverse container space-x-7 space-x-reverse items-center">
-          <button
-            onClick={openMenu}
-            className="max-sm:mx-4 max-sm:w-12 max-sm:h-12 max-lg:w-14 max-lg:h-14 w-16 h-16 bg-purple-700 rounded-full flex items-center justify-center border-4 border-gray-700 "
-          >
-            <span className="text-2xl font-medium text-white">
-              {userName[0]}
-            </span>
-          </button>
-        </div>
+        <div className="flex flex-row-reverse container space-x-7 space-x-reverse items-center">
+          <div onMouseLeave={closeMenu}>
+            <button
+              onClick={openMenu}
+              className="max-sm:mx-4 max-sm:w-12 max-sm:h-12 max-lg:w-14 max-lg:h-14 w-16 h-16 bg-purple-700 rounded-full items-center justify-center border-4 border-gray-700 "
+            >
+              <span className="text-2xl font-medium text-white">
+                {userName[0]}
+              </span>
+            </button>
 
-        <div
-          onMouseLeave={closeMenu}
-          className={`${menuStatus ? 'block' : 'hidden'} h-fit w-fit`}
-        >
-          <div
-            className="mt-2 rounded-md bg-gray-700 text-white"
-            role="menu"
-            aria-orientation="vertical"
-            aria-labelledby="options-menu"
-          >
-            <a
-              href=""
-              className="max-sm:text-xs  block px-4 py-2 text-white hover:bg-gray-900 rounded-md"
-              role="menuitem"
+            <div
+              className={`${
+                menuStatus ? 'block' : 'hidden'
+              } h-fit w-fit absolute`}
             >
-              Editar Perfil
-            </a>
-            <a
-              href=""
-              onClick={logOut}
-              className="max-sm:text-xs block px-4 py-2 text-white hover:bg-gray-900 rounded-md"
-              role="menuitem"
-            >
-              Sair
-            </a>
+              <div
+                className="rounded-md bg-gray-700 text-white"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="options-menu"
+              >
+                <a
+                  href=""
+                  className="max-sm:text-xs  block px-4 py-2 text-white hover:bg-gray-900 rounded-md"
+                  role="menuitem"
+                >
+                  Editar Perfil
+                </a>
+                <a
+                  href=""
+                  onClick={logOut}
+                  className="max-sm:text-xs block px-4 py-2 text-white hover:bg-gray-900 rounded-md"
+                  role="menuitem"
+                >
+                  Sair
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </DefaultHeader>
--- src/components/DefaultHeader/index.tsx
+++ src/components/DefaultHeader/index.tsx
@@ -1,6 +1,9 @@
-export default function DefalutHeader({ children }: any) {
+export default function DefalutHeader({ children, onMouseLeave }: any) {
   return (
-    <div className="max-sm:h-14 max-lg:h-16 h-20 bg-[#121214] border-b border-gray-800 flex items-center">
+    <div
+      onMouseLeave={onMouseLeave}
+      className="max-sm:h-14 max-lg:h-16 h-20 bg-[#121214] border-b border-gray-800 flex items-center"
+    >
       {children}
     </div>
   );
